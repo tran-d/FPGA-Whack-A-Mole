bd4964d7fe9974ba891d58f82ac74cbe38e59d38
Raj Borra <rkb15@duke.edu>
2018-04-12 15:40:07 -0400

    added led array

--- led_controller/led.v
+++ led_controller/led.v
@@ -1,23 +1,23 @@
-module led(clock, code, pin_r, pin_g);
+module led(clock, code, pin_r, pin_b);
 
 	input clock;
 	input [15:0] code;
-	output pin_r, pin_g;
+	output pin_r, pin_b;
 	
-	reg [7:0] red, green;
+	reg [7:0] red, blue;
 	
 	initial begin
 		red <= 8'b0;
-		green <= 8'b0;
+		blue <= 8'b0;
 	end
 	
 	always @(posedge clock) begin
 		red <= code[15:8];
-		green <= code[7:0];	
+		blue <= code[7:0];	
 	end
 	
 	pwm red_signal(.clk(clock), .pwm_codeword(red), .pwm_out(pin_r));
-	pwm green_signal(.clk(clock), .pwm_codeword(green), .pwm_out(pin_g));
+	pwm blue_signal(.clk(clock), .pwm_codeword(blue), .pwm_out(pin_b));
 	
 endmodule
 
