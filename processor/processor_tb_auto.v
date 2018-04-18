dff6f51fa44ff651cf0f81c483d1f891efa3219f
Dhruv K Patel <dkp13@duke.edu>
2018-04-17 21:04:26 -0400

    added compiler encapsulated variable support

--- processor/processor_tb_auto.v
+++ processor/processor_tb_auto.v
@@ -25,7 +25,7 @@ module processor_tb_auto(
     capacitive_sensors_in,
 	capacitive_sensors_out);
 
-	integer CYCLE_LIMIT = 400; // Modify this to change number of cycles run during test
+	integer CYCLE_LIMIT = 1000; // Modify this to change number of cycles run during test
 
 	reg clock = 0, reset = 0;
 	integer cycle_count = 0, error_count = 0;
@@ -265,13 +265,9 @@ module processor_tb_auto(
 	endtask
 
 	task performTests; begin
-		checkRegister(32'd3, 32'd5);
-		checkRegister(32'd4, -32'd5);
-		checkRegister(32'd5, -32'd486);
-		checkRegister(32'd6, 32'd0);
-		checkRegister(32'd7, 32'd0);
-		checkRegister(32'd8, 32'd486);
-		checkRegister(32'd9, 32'd0);
+		checkRegister(32'd1, 32'd20);
+		checkRegister(32'd2, 32'd20);
+		checkRegister(32'd3, 32'd40);
 	end endtask
 
 endmodule
